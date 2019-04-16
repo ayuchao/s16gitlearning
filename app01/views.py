a79eb60a95fae4ea860063333bb2157151557229
xiaofeng <yc_uuu@163.com>
2019-04-16 10:46:14 +0800

    超哥提交了躺下真舒服那个代码

--- app01/views.py
+++ app01/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render,HttpResponse
 def happy(request):
     return HttpResponse("我问王思聪,钱是万能的吗,王思聪说:钱是万达的,修复了这个bug,钱是老男孩的了")
 
+
+def happy2(reuqest):
+    return HttpResponse("曾经我被人打趴下了,然后我发现躺着真舒服")
 # Create your views here.
