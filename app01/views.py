9d3bb251926f2c3770d0d8d696d099ced68d208c
xiaofeng <yc_uuu@163.com>
2019-04-16 10:40:22 +0800

    超哥修复了bug,提交了新的代码

--- app01/views.py
+++ app01/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,HttpResponse
 
 def happy(request):
-    return HttpResponse("我问王思聪,钱是万能的吗,王思聪说:钱是万达的")
+    return HttpResponse("我问王思聪,钱是万能的吗,王思聪说:钱是万达的,修复了这个bug,钱是老男孩的了")
 
 # Create your views here.
